707b06e16f387068b7196908dd7d304e157b8e44
Crowdbotics <team@crowdbotics.com>
2023-07-26 15:47:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sanofi-demo-42440",
+  url: "https://sanofi-demo-42440.botics.co",
+  api: "https://sanofi-demo-42440.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
